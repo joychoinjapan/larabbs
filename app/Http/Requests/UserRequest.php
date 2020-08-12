09983efb4b0100ff4ba8d43977ba4a16886385e2
miaobaobao <joychoinjapan@gmail.com>
2020-08-12 10:02:11 +0900

    プロフィール画像更新機能のバリデーション

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -28,6 +28,7 @@ class UserRequest extends FormRequest
             'name' => 'required|between:3,25|regex:/^[A-Za-z0-9\-\_]+$/|unique:users,name,' . Auth::id(),
             'email' => 'required|email',
             'introduction' => 'max:80',
+            'avatar'=>'mimes:jpeg,bmp,png,gif|dimensions:min_width=208,min_height=208'
         ];
     }
 
@@ -35,10 +36,12 @@ class UserRequest extends FormRequest
     public function messages()
     {
         return [
-            'name.unique'=>'ユーザー名はすでに使用されております。',
-            'name.regex'=>'ユーザー名は英数字、または"_""/""-"を使用してください。',
-            'name.between'=>'ユーザー名の文字数を3から25まで設定してください。',
-            'name.required'=>'ユーザー名を入力してください。'
+            'name.unique'=>'ユーザー名はすでに使用されております',
+            'name.regex'=>'ユーザー名は英数字、または"_""/""-"を使用してください',
+            'name.between'=>'ユーザー名の文字数を3から25まで設定してください',
+            'name.required'=>'ユーザー名を入力してください。',
+            'avatar.mimes'=>'画像はjpeg,bmp,png,gifの形式を使用してください',
+            'avatar.dimensions'=>'横幅と縦幅が208px以上の画像を使用してください'
         ];
     }
 }
