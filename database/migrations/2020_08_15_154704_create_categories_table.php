c8a27f7c050963380bafa33149ae883781545169
miaobaobao <joychoinjapan@gmail.com>
2020-08-15 16:27:02 +0900

    category

---
+++ database/migrations/2020_08_15_154704_create_categories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name')->index()->comment('名前');
+            $table->text('description')->nullable()->comment('記述');
+            $table->integer('post_count')->default(0)->comment('ポストの数');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+}
---
+++ database/migrations/2020_08_15_161925_seed_categories_data.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class SeedCategoriesData extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        $categories = [
+            [
+                'name' => 'シェア',
+                'description' => 'ノーハウをシェア'
+            ],
+            [
+                'name' => 'コース',
+                'description' => '開発の技術を教える'
+            ],
+            [
+                'name' => '質問回答',
+                'description' => 'お互いに助け合い'
+            ],
+            [
+                'name' => 'お知らせ',
+                'description' => '運営のお知らせ'
+            ],
+        ];
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
