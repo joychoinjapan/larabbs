2472a2b2d03c34763aa5f2cd204c4a0430b8ac23
miaobaobao <joychoinjapan@gmail.com>
2020-08-19 15:08:25 +0900

    "add topic list"

--- app/Models/Topic.php
+++ app/Models/Topic.php
@@ -4,5 +4,17 @@ namespace App\Models;
 
 class Topic extends Model
 {
-    protected $fillable = ['title', 'body', 'user_id', 'category_id', 'view_count', 'last_reply_user_id', 'order', 'excerpt', 'slug'];
+    protected $fillable = ['title', 'body', 'user_id','reply_count',
+        'category_id', 'view_count', 'last_reply_user_id',
+        'order', 'excerpt', 'slug'];
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
--- database/factories/TopicFactory.php
+++ database/factories/TopicFactory.php
@@ -1,9 +1,24 @@
 <?php
 
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Topic;
 use Faker\Generator as Faker;
 
-$factory->define(App\Models\Topic::class, function (Faker $faker) {
+$factory->define(\App\Models\Topic::class, function (Faker $faker) {
+    $sentence = $faker->sentence();
+    //随机取一个月以内的时间
+    $updated_at = $faker->dateTimeThisMonth();
+    //为创建时间传参，意为最大不超过 $updated_at
+    $created_at = $faker->dateTimeThisMonth($updated_at);
+
     return [
-        // 'name' => $faker->name,
+        'title' => $sentence,
+        'body' => $faker->text(),
+        'excerpt' => $sentence,
+        'user_id' => $faker->randomElement([1,2,3,4,5,6,7,8,9,10]),
+        'category_id' => $faker->randomElement([1,2,3,4]),
+        'created_at' => $created_at,
+        'updated_at' => $updated_at
     ];
 });
