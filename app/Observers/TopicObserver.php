952953f143ff0175972844672dfca2401bc08503
miaobaobao <joychoinjapan@gmail.com>
2020-08-27 02:38:01 +0900

    redis queue 実装。課題あり

--- app/Observers/TopicObserver.php
+++ app/Observers/TopicObserver.php
@@ -3,6 +3,7 @@
 namespace App\Observers;
 
 use App\Handlers\SlugTranslateHandler;
+use App\Jobs\TranslateSlug;
 use App\Models\Topic;
 
 // creating, created, updating, updated, saving,
@@ -23,21 +24,25 @@ class TopicObserver
     public function saving(Topic $topic)
     {
         $topic->excerpt = make_excerpt($topic->body);
-        if(! $topic->slug){
-            $result = app(SlugTranslateHandler::class)->translate($topic->title);
+//        if(! $topic->slug){
+//            $result = app(SlugTranslateHandler::class)->translate($topic->title);
+//
+//            if(!is_array($result)){
+//                $result=json_decode($result,true);
+//            }
+//
+//            if(array_key_exists('code',$result)&&400==$result['code']){
+//                session()->flash('danger',$result['message']);
+//                return back();
+//            }
+//
+//            if(array_key_exists('text',$result)){
+//                $topic->slug = str_replace(' ','-',$result['text']);
+//            }
+//        }
 
-            if(!is_array($result)){
-                $result=json_decode($result,true);
-            }
-
-            if(array_key_exists('code',$result)&&400==$result['code']){
-                session()->flash('danger',$result['message']);
-                return back();
-            }
-
-            if(array_key_exists('text',$result)){
-                $topic->slug = str_replace(' ','-',$result['text']);
-            }
+        if(!$topic->slug){
+            dispatch(new TranslateSlug($topic));
         }
     }
 }
